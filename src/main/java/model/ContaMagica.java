7be01c0da09f5f16717a382288fdf68b7b2f796f
danielle_madrid <daniellereismadridd@gmail.com>
2021-12-13 15:34:22 -0300

    Desenvolvendo os testes

--- src/main/java/model/ContaMagica.java
+++ src/main/java/model/ContaMagica.java
@@ -3,6 +3,7 @@ package model;
 import enums.Categoria;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class ContaMagica {
     private String cliente;
@@ -41,50 +42,40 @@ public class ContaMagica {
         this.categoria = categoria;
     }
 
+    //
     public void deposita(BigDecimal valor) {
-        String deposita = valor.toString();
-        if (getStatus().equals(Categoria.GOLD)) {
-            BigDecimal valorDeposita = valor;
-            valor = valor.multiply(new BigDecimal(0.01));
-            valor = valor.add(valorDeposita);
-            saldo = new BigDecimal(deposita).add(valor);
-        } else if (getStatus().equals(Categoria.PLATINUM)) {
-            BigDecimal valorDeposita = valor;
-            valor = valor.multiply(new BigDecimal(0.025));
-            valor = valor.add(valorDeposita);
-            saldo = new BigDecimal(deposita).add(valor);
+        setSaldo(saldo.add(valor));
+        int gold = saldo.compareTo(new BigDecimal("50000"));
+        int platinium = saldo.compareTo(new BigDecimal("200000"));
+
+        if ((gold > 0) || (gold == 0)) {
+            setCategoria(Categoria.GOLD);
+            System.out.println("Cliente Gold");
+            setSaldo(saldo.add(valor.multiply(new BigDecimal("0.01"))));
         }
-
+        if ((platinium > 0) || (platinium == 0)) {
+            setCategoria(Categoria.PLATINUM);
+            setSaldo(saldo.add(valor.multiply(new BigDecimal("0.025"))));
+            System.out.println("Cliente Platinum");
+        }
     }
-
 
     public void retirada(BigDecimal valor) {
-        String retirada = valor.toString();
-        saldo = new BigDecimal(retirada).subtract(valor);
-
-        if (saldo.compareTo(new BigDecimal(100000)) == -1) {
-            categoria = Categoria.GOLD;
-            System.out.println("Você perdeu sua categoria de Platinum e passou para Gold");
-
-        } else if (saldo.compareTo(new BigDecimal(25000)) == -1) {
-            categoria = Categoria.SILVER;
-            System.out.println("Você perdeu sua categoria de Gold e passou para Silver");
-
+        int verificaSaldo = saldo.compareTo(valor);
+        if (verificaSaldo >= 0) {
+            setSaldo(saldo.subtract(valor));
+            if ((getSaldo().compareTo(new BigDecimal("100000")) < 0) && getCategoria() == Categoria.PLATINUM) {
+                setCategoria(Categoria.GOLD);
+                System.out.println("De platinum você foi para gold");
+            } else if ((getSaldo().compareTo(new BigDecimal("25000")) < 0) && getCategoria() == Categoria.GOLD) {
+                setCategoria(Categoria.SILVER);
+                System.out.println("De Gold você foi para Silver");
+            }
         }
     }
 
-
     public Categoria getStatus() {
-        BigDecimal saldoAtual = saldo;
-
-        if (saldoAtual.compareTo(new BigDecimal(50000)) == -1) {
-            categoria = Categoria.SILVER;
-        } else if (saldoAtual.compareTo(new BigDecimal(50000)) == 0 || (saldoAtual.compareTo(new BigDecimal(50000)) == 1) && saldoAtual.compareTo(new BigDecimal(20000)) == -1) {
-            categoria = Categoria.GOLD;
-        } else {
-            categoria = Categoria.PLATINUM;
-        }
-        return categoria;
+        return getCategoria();
     }
 
 }
--- src/test/java/ContaMagicaTest.java
+++ src/test/java/ContaMagicaTest.java
@@ -1,29 +1,104 @@
+import enums.Categoria;
 import model.ContaMagica;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 
 public class ContaMagicaTest {
-    //1-testar a retirada com cada categoria
-    //2- cliente silver não é valorizado com bonus
-    //3- cliente tem que inicar na categoria silver
-    //4- cliente platinum tem que depositar e receber o bonus 2,5% sobre o valor de deposito
-    //5- cliente gold tem que depositar e receber o bonus de 1% sobre o valor de deposito
-    //6- se o platinum tiver saldo abaixo de 100mil ele desce pra categoria gold
-    //7- se o gold tiver saldo abaixo de 25mil ele desce pra categoria silver
-    //8- nao conseguir retirar se o saldo for 0
-    //9- nao pode depositar valor negativo
-    //10- quando o saldo ultrapassa 50mil a categoria vira gold
-    //11- quando o saldo ultrapassa 200mil a categoria vira platinum
-
-
-
-
     private ContaMagica contaMagica;
 
+    @BeforeEach
+    public void setUp() {
+        contaMagica = new ContaMagica("dani", new BigDecimal(1000), Categoria.SILVER);
+
+    }
+
     @Test
-    public void deveriaDepositar(){
-        contaMagica.deposita(new BigDecimal(10000));
+    public void deveriaDepositar() {
+        System.out.println("depositando:");
+        contaMagica.deposita(new BigDecimal("4000"));
+        assertEquals(new BigDecimal("5000"), contaMagica.getSaldo());
+        System.out.println(contaMagica.getSaldo());
+
+    }
+    @Test
+    public void deveriaRetirar() {
+        System.out.println("retirando:");
+        contaMagica.deposita(new BigDecimal("40000"));
+        contaMagica.retirada(new BigDecimal("20000"));
+        assertEquals(new BigDecimal("21000"), contaMagica.getSaldo());
         System.out.println(contaMagica.getSaldo());
     }
-}
+
+    @Test
+    public void deveriaSerPlatinumComBonus() {
+        contaMagica.deposita(new BigDecimal("200000"));
+        System.out.println(contaMagica.getSaldo());
+        assertEquals(Categoria.PLATINUM, contaMagica.getCategoria());
+    }
+    @Test
+    public void deveriaSerGoldComBonus(){
+        contaMagica.deposita(new BigDecimal("50000"));
+        contaMagica.deposita(new BigDecimal("20000"));
+        contaMagica.deposita(new BigDecimal("10"));
+        System.out.println(contaMagica.getSaldo());
+        assertEquals(Categoria.GOLD, contaMagica.getCategoria());
+    }
+    @Test
+    public void deveriaPassarGoldParaPlatinum(){
+        contaMagica.deposita(new BigDecimal("200000"));
+        contaMagica.retirada(new BigDecimal("1000"));
+        System.out.println(contaMagica.getSaldo());
+        assertEquals(Categoria.PLATINUM, contaMagica.getCategoria());
+
+    }
+    @Test
+    void deveriaPassarPlatinumParaGold() {
+        contaMagica.deposita(new BigDecimal("200000"));
+        contaMagica.retirada(new BigDecimal("200000"));
+        System.out.println(contaMagica.getSaldo());
+        assertEquals(Categoria.GOLD, contaMagica.getCategoria());
+    }
+
+    @Test
+    void deveAtualizarDeGoldParaSilver() {
+        contaMagica.deposita(new BigDecimal("50000"));
+        contaMagica.retirada(new BigDecimal("40000"));
+        System.out.println(contaMagica.getSaldo());
+        assertEquals(Categoria.SILVER,contaMagica.getCategoria());
+    }
+
+    @Test
+    public void naoDeveriaRetirarSaldoZero() {
+        contaMagica = new ContaMagica("dani", new BigDecimal(0), Categoria.SILVER);
+        contaMagica.retirada(new BigDecimal("10"));
+        assertEquals(new BigDecimal("0"), contaMagica.getSaldo());
+    }
+
+    @Test
+    public void naoDeveriaDepositarValorNegativo(){
+        contaMagica.deposita(new BigDecimal("-10"));
+       assertEquals(new BigDecimal("1000"), contaMagica.getSaldo());
+    }
+    @Test
+    public void naoDeveriaRetirarSaldoQueNãoTem(){
+        contaMagica.deposita(new BigDecimal(2000));
+        contaMagica.retirada(new BigDecimal(3000));
+        assertEquals(new BigDecimal(0), contaMagica.getSaldo());
+        System.out.println(contaMagica.getStatus());
+    }
+ }
+
+//1-testar se a retirada está funcionando
+//2- testar se o deposita está funcionando
+//3- cliente platinum tem que depositar e receber o bonus 2,5% sobre o valor de deposito
+//5- cliente gold tem que depositar e receber o bonus de 1% sobre o valor de deposito
+//6- se o platinum tiver saldo abaixo de 100mil ele desce pra categoria gold
+//7- se o gold tiver saldo abaixo de 25mil ele desce pra categoria silver
+//8- nao conseguir retirar se o saldo for 0
+//9- nao pode depositar valor negativo
+//10- quando o saldo ultrapassa 50mil a categoria vira gold
+//11- quando o saldo ultrapassa 200mil a categoria vira platinum
